a76be9b37f87eaf2795522ace63a91b97b9b6d39
Notinctemps <marc.balaguer.quintana@alumnes.ilernabcn.com>
2023-09-20 11:48:57 +0200

    Exercici acabat

---
+++ 20.09.23_Entorn_Servidor/javascrypt.js
@@ -0,0 +1,27 @@
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onload = function() {myFunction(this);}
+    xhttp.open("GET", "cd_catalog.xml");
+    xhttp.send();
+  }
+  function myFunction(xml) {
+    const xmlDoc = xml.responseXML;
+    const x = xmlDoc.getElementsByTagName("CD");
+    let table="<tr><th>Artist</th><th>Title</th><th>Country</th><th>Company</th><th>Price</th><th>Year</th></tr>";
+    for (let i = 0; i <x.length; i++) {
+      table += "<tr><td>" +
+            x[i].getElementsByTagName("ARTIST")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("TITLE")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("COUNTRY")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("COMPANY")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("PRICE")[0].childNodes[0].nodeValue +
+            "</td><td>" +
+            x[i].getElementsByTagName("YEAR")[0].childNodes[0].nodeValue +
+            "</td></tr>";
+    }
+    document.getElementById("demo").innerHTML = table;
+  }
